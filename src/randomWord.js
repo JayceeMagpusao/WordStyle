d22769364649eba22a8ad83fa6ce02b94567e6e0
jaycee <jay27mag@yahoo.com>
2021-07-13 00:43:59 -0700

    pushing through instrumental

--- src/randomWord.js
+++
@@ -1,7 +0,0 @@
-let nextWord=document.querySelector(".word");
-let para=document.querySelector("#word p");
-
-let words = ['ice cream', 'pizza', 'mozarella'];
-nextWord.addEventListener('click', () => {
-  para.innerHTML = word[Math.floor(Math.random()*word.length)];
-})
